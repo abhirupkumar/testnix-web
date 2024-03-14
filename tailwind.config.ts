97cbaf70ab6f05b950835d9d62263c03934b2376
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-03-14 22:12:40 +0530

    changed tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}'
   ],
   prefix: "",
   theme: {
