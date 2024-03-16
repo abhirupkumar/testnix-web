f3b2b241ec43d016c42d31de54995cb58b32d9a1
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-03-16 22:02:38 +0530

    Added recharts

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,93 +1,89 @@
 "use client";
 
-import { AreaChart } from '@tremor/react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import React, { useEffect, useState } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
-const chartdata = [
+const data = [
     {
-        date: 'Jan 22',
-        SemiAnalysis: 2890,
-        'The Pragmatic Engineer': 2338,
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
     },
     {
-        date: 'Feb 22',
-        SemiAnalysis: 2756,
-        'The Pragmatic Engineer': 2103,
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
     },
     {
-        date: 'Mar 22',
-        SemiAnalysis: 3322,
-        'The Pragmatic Engineer': 2194,
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
     },
     {
-        date: 'Apr 22',
-        SemiAnalysis: 3470,
-        'The Pragmatic Engineer': 2108,
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
     },
     {
-        date: 'May 22',
-        SemiAnalysis: 3475,
-        'The Pragmatic Engineer': 1812,
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
     },
     {
-        date: 'Jun 22',
-        SemiAnalysis: 3129,
-        'The Pragmatic Engineer': 1726,
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
     },
     {
-        date: 'Jul 22',
-        SemiAnalysis: 3490,
-        'The Pragmatic Engineer': 1982,
-    },
-    {
-        date: 'Aug 22',
-        SemiAnalysis: 2903,
-        'The Pragmatic Engineer': 2012,
-    },
-    {
-        date: 'Sep 22',
-        SemiAnalysis: 2643,
-        'The Pragmatic Engineer': 2342,
-    },
-    {
-        date: 'Oct 22',
-        SemiAnalysis: 2837,
-        'The Pragmatic Engineer': 2473,
-    },
-    {
-        date: 'Nov 22',
-        SemiAnalysis: 2954,
-        'The Pragmatic Engineer': 3848,
-    },
-    {
-        date: 'Dec 22',
-        SemiAnalysis: 3239,
-        'The Pragmatic Engineer': 3736,
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
     },
 ];
 
-const valueFormatter = function (number: number | bigint) {
-    return '$ ' + new Intl.NumberFormat('us').format(number).toString();
-};
-
 export default function Chart() {
+    const [width, setWidth] = useState(window.innerWidth)
+    const handleResize = () => setWidth(window.innerWidth)
+    useEffect(() => {
+        handleResize()
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
-        <>
-            <Card className="w-full mt-6">
-                <CardHeader>
-                    <CardTitle>History </CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <AreaChart
-                        className="mt-4 h-72 text-white"
-                        data={chartdata}
-                        index="date"
-                        yAxisWidth={65}
-                        categories={['SemiAnalysis', 'The Pragmatic Engineer']}
-                        valueFormatter={valueFormatter}
-                    />
-                </CardContent>
-            </Card>
-        </>
+        <Card className="w-full mt-6">
+            <CardHeader>
+                <CardTitle>History </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <AreaChart
+                    width={width >= 1280 ? 1050 : width - 280}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <XAxis dataKey="amt" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="uv" />
+                    <Area type="monotone" dataKey="pv" />
+                </AreaChart>
+
+            </CardContent>
+        </Card>
     );
 }
