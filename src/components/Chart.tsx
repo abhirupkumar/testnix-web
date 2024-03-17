152192a219223ceba8b213ccb585e0ce890bdb73
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-03-17 20:40:00 +0530

    commit

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,63 +1,72 @@
-"use client";
-
-import React, { useEffect, useState } from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart } from '@tremor/react';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
-const data = [
+const chartdata = [
     {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        date: 'Jan 22',
+        SemiAnalysis: 2890,
+        'The Pragmatic Engineer': 2338,
     },
     {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+        date: 'Feb 22',
+        SemiAnalysis: 2756,
+        'The Pragmatic Engineer': 2103,
     },
     {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+        date: 'Mar 22',
+        SemiAnalysis: 3322,
+        'The Pragmatic Engineer': 2194,
     },
     {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+        date: 'Apr 22',
+        SemiAnalysis: 3470,
+        'The Pragmatic Engineer': 2108,
     },
     {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
+        date: 'May 22',
+        SemiAnalysis: 3475,
+        'The Pragmatic Engineer': 1812,
     },
     {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
+        date: 'Jun 22',
+        SemiAnalysis: 3129,
+        'The Pragmatic Engineer': 1726,
     },
     {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
+        date: 'Jul 22',
+        SemiAnalysis: 3490,
+        'The Pragmatic Engineer': 1982,
+    },
+    {
+        date: 'Aug 22',
+        SemiAnalysis: 2903,
+        'The Pragmatic Engineer': 2012,
+    },
+    {
+        date: 'Sep 22',
+        SemiAnalysis: 2643,
+        'The Pragmatic Engineer': 2342,
+    },
+    {
+        date: 'Oct 22',
+        SemiAnalysis: 2837,
+        'The Pragmatic Engineer': 2473,
+    },
+    {
+        date: 'Nov 22',
+        SemiAnalysis: 2954,
+        'The Pragmatic Engineer': 3848,
+    },
+    {
+        date: 'Dec 22',
+        SemiAnalysis: 3239,
+        'The Pragmatic Engineer': 3736,
     },
 ];
 
 export default function Chart() {
-    const [width, setWidth] = useState(0)
-    const handleResize = () => setWidth(window.innerWidth)
-    useEffect(() => {
-        handleResize()
-        window.addEventListener('resize', handleResize)
-        return () => window.removeEventListener('resize', handleResize)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+
+    const colorsArray = ["red", "blue", "yellow", "green", "orange", "amber", "cyan", "lime", "emerald", "teal", "sky", "indigo", "violet", "purple", "fuchsia", "pink", "rose", "slate", "gray", "zinc", "neutral", "stone",];
 
     return (
         <Card className="w-full mt-6">
@@ -66,23 +75,12 @@ export default function Chart() {
             </CardHeader>
             <CardContent>
                 <AreaChart
-                    width={width >= 1280 ? 1050 : width - 280}
-                    height={400}
-                    data={data}
-                    margin={{
-                        top: 10,
-                        right: 30,
-                        left: 0,
-                        bottom: 0,
-                    }}
-                >
-                    <XAxis dataKey="amt" />
-                    <YAxis />
-                    <Tooltip />
-                    <Area type="monotone" dataKey="uv" />
-                    <Area type="monotone" dataKey="pv" />
-                </AreaChart>
-
+                    data={chartdata}
+                    index="date"
+                    categories={['SemiAnalysis', 'The Pragmatic Engineer']}
+                    colors={[colorsArray[0], colorsArray[1]]}
+                    yAxisWidth={60}
+                />
             </CardContent>
         </Card>
     );
