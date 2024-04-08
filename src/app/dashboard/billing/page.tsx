bc09040acd333ec9b7c6d7e4b5a96f8a71bc6884
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-04-08 20:30:13 +0530

    added safety measures

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import BillingForm from '@/components/BillingForm';
 import { getUserSubscriptionPlan } from '@/lib/stripe';
+import { getCurrentUser } from '@/lib/firebase-admin';
+import { redirect } from 'next/navigation';
+import { absoluteUrl } from '@/lib/utils';
 
 const Page = async () => {
+    const currentUser = await getCurrentUser();
     const subscriptionPlan = await getUserSubscriptionPlan();
+    if (!currentUser) redirect(absoluteUrl("/sign-in"))
 
     return <BillingForm subscriptionPlan={subscriptionPlan} />
 }
