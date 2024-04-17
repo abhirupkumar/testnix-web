f755c911316173eb00b8ff865ad805aff838e097
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-04-17 17:27:49 +0530

    fixs

--- next.config.mjs
+++ next.config.mjs
@@ -12,23 +12,14 @@ const nextConfig = {
                 ],
             },
             {
-                source: "/api/v1/event-conversions",
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ],
-            },
-            {
-                source: "/api/v1/event-clicks",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ],
-            },
+            }
         ];
     },
     images: {
