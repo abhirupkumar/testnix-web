13c1a6aae94b37376ec25af0b59bcb13750188e9
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-04-17 13:01:04 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -12,15 +12,6 @@ const nextConfig = {
                 ],
             },
             {
-                source: "/api/v1/variant",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ],
-            },
-            {
                 source: "/api/v1/event-conversions",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
